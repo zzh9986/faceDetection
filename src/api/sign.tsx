496132a261bf52194544d476ef1f25bbafcd20ca
zzh9986 <1067873610@qq.com>
2020-04-15 13:33:11 +0800

    feat: modify interface

--- src/api/sign.tsx
+++ src/api/sign.tsx
@@ -1,9 +1,13 @@
 import { $ajax } from './post';
 
 export function signIn(params?) {
-	return $ajax.$post("http://38gq8y.natappfree.cc/login1", params);
+	return $ajax.$post("http://95xtta.natappfree.cc/login1", params);
 }
 
 export function signUp(params?) {
-	return $ajax.$post("http://38gq8y.natappfree.cc/register1", params);
+	return $ajax.$post("http://95xtta.natappfree.cc/register1", params);
+}
+
+export function getTeacherInfo(params?) {
+	return $ajax.$post("http://95xtta.natappfree.cc/teacherdetail1", params);
 }
--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -1,13 +1,34 @@
 import * as React from "react";
+import { getTeacherInfo } from "../../api";
 import "./index.scss";
 
 export default props => {
-	// const { useState } = React;
+	const { useState, useEffect } = React;
+	const [name, setName] = useState<string>("");
+
+	const getInfo = () => {
+		const params = {
+			user_id: localStorage.getItem("user_id")
+		}
+		getTeacherInfo(params).then((res) => {
+			console.log(res)
+			if (res.data.status === 0) {
+				setName(res.data.teacher_name)
+			}
+		}).catch((err) => {
+			console.log(err)
+		})
+	}
+
+	useEffect(() => {
+		getInfo()
+	}, [])
+
 	console.log(props)
 	return (
 		<>
 			<div className="header">
-				<p className="tip">您好,史潇哲</p>
+				<p className="tip">您好，{name}</p>
 				<a className="signout">退出</a>
 			</div>
 		</>
