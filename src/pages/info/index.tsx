835fb03c37a409ccdb7fcc8a5a1d8dbe82d1d7d7
zzh9986 <1067873610@qq.com>
2020-04-15 13:22:14 +0800

    feat: add info module header

--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
+import "./index.scss";
 
 export default props => {
 	// const { useState } = React;
 	console.log(props)
 	return (
 		<>
-			<div>
-				info
+			<div className="header">
+				<p className="tip">您好,史潇哲</p>
+				<a className="signout">退出</a>
 			</div>
 		</>
 	);
