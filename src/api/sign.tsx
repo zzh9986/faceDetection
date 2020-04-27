ba356bd84b7a098997bc6a82632025e900258115
zzh9986 <1067873610@qq.com>
2020-04-27 20:49:35 +0800

    feat: modify ajax

--- src/api/sign.tsx
+++ src/api/sign.tsx
@@ -1,21 +1,21 @@
 import { $ajax } from './post';
 
 export function signIn(params?) {
-	return $ajax.$post("http://diyf3r.natappfree.cc/login1", params);
+	return $ajax.$post("http://zfajb7.natappfree.cc/login1", params);
 }
 
 export function signUp(params?) {
-	return $ajax.$post("http://diyf3r.natappfree.cc/register1", params);
+	return $ajax.$post("http://zfajb7.natappfree.cc/register1", params);
 }
 
 export function getTeacherInfo(params?) {
-	return $ajax.$post("http://diyf3r.natappfree.cc/teacherdetail1", params);
+	return $ajax.$post("http://zfajb7.natappfree.cc/teacherdetail1", params);
 }
 
 export function getCourseInfo(params?) {
-	return $ajax.$post("http://diyf3r.natappfree.cc/coursedetail1", params);
+	return $ajax.$post("http://zfajb7.natappfree.cc/coursedetail1", params);
 }
 
 export function getAdminInfo(params?) {
-	return $ajax.$post("http://diyf3r.natappfree.cc/admindetail1", params)
+	return $ajax.$post("http://zfajb7.natappfree.cc/admindetail1", params)
 }
